466219ff333e150a3cdee560cbf9bd2b7e5a30e5
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2022-11-04 12:30:10 +0100

    Add FirstLastComparator utility class.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

---
+++ src/main/java/nl/talsmasoftware/misc/utils/FirstLastComparator.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2022 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.misc.utils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Comparator to sort certain values first or last.
+ * <p>
+ * For example:
+ * <pre>{@code
+ * public static final Comparator<String> ADJUSTED_ORDER =
+ *     FirstLastComparator.compareLast(String.CASE_INSENSITIVE_ORDER, "Always last");
+ * }</pre>
+ * In the example above, {@code ADJUSTED_ORDER} will sort Strings case-insensitive
+ * where {@code "Always last"} is always sorted last.
+ *
+ * @param <T> The type to be sorted.
+ */
+public final class FirstLastComparator<T> implements Comparator<T>, Serializable {
+    private final Comparator<T> delegate;
+
+    private final boolean compareFirst;
+
+    private final List<T> explicitValues;
+
+    private FirstLastComparator(Comparator<T> delegate, boolean compareFirst, Collection<? extends T> explicitValues) {
+        this.delegate = Objects.requireNonNull(delegate, "Delegate comparator is <null>.");
+        this.compareFirst = compareFirst;
+        this.explicitValues = new ArrayList<>(explicitValues);
+    }
+
+    /**
+     * Sort a limited collection of values first, delegating main sorting to another comparator.
+     *
+     * @param delegate    The main sorting delegate for all 'other' values.
+     * @param firstValues The values to be sorted first.
+     * @param <T>         The type to be sorted.
+     * @return A comparator sorting the given values first.
+     */
+    public static <T> Comparator<T> compareFirst(Comparator<T> delegate, Collection<? extends T> firstValues) {
+        return new FirstLastComparator<>(delegate, true,
+                Objects.requireNonNull(firstValues, "firstValues is <null>."));
+    }
+
+    /**
+     * Sort one or more values first, delegating main sorting to another comparator.
+     *
+     * @param delegate    The main sorting delegate for all 'other' values.
+     * @param firstValues The values to be sorted first.
+     * @param <T>         The type to be sorted.
+     * @return A comparator sorting the given values first.
+     */
+    @SafeVarargs
+    public static <T> Comparator<T> compareFirst(Comparator<T> delegate, T... firstValues) {
+        return compareFirst(delegate, firstValues != null ? Arrays.asList(firstValues) : null);
+    }
+
+    /**
+     * Sort a limited collection of values last, delegating main sorting to another comparator.
+     *
+     * @param delegate   The main sorting delegate for all 'other' values.
+     * @param lastValues The values to be sorted last.
+     * @param <T>        The type to be sorted.
+     * @return A comparator sorting the given values last.
+     */
+    public static <T> Comparator<T> compareLast(Comparator<T> delegate, Collection<? extends T> lastValues) {
+        return new FirstLastComparator<>(delegate, false,
+                Objects.requireNonNull(lastValues, "lastValues is <null>."));
+    }
+
+    /**
+     * Sort one or more values last, delegating main sorting to another comparator.
+     *
+     * @param delegate   The main sorting delegate for all 'other' values.
+     * @param lastValues The values to be sorted last.
+     * @param <T>        The type to be sorted.
+     * @return A comparator sorting the given values last.
+     */
+    @SafeVarargs
+    public static <T> Comparator<T> compareLast(Comparator<T> delegate, T... lastValues) {
+        return compareLast(delegate, lastValues != null ? Arrays.asList(lastValues) : null);
+    }
+
+    /**
+     * Compare two objects.
+     * <p>
+     * Check whether the compared objects are in the limited set of specified values.
+     * If this is the case, the corresponding object(s) is sorted either first or last,
+     * depending on the used factory method ({@code compareFirst} vs. {@code compareLast}).<br>
+     * If neither of the compared objects are explicitly sorted first or last,
+     * the specified {@code delegate} Comparator is used.
+     *
+     * @param o1 the first object to be compared.
+     * @param o2 the second object to be compared.
+     * @return a negative integer, zero, or a positive integer
+     * as the first argument is less than, equal to, or greater than the second.
+     */
+    @Override
+    public int compare(T o1, T o2) {
+        final int i1 = explicitValues.indexOf(o1);
+        final int i2 = explicitValues.indexOf(o2);
+
+        if (i1 < 0) {
+            if (i2 < 0) return delegate.compare(o1, o2);
+            else return compareFirst ? 1 : -1;
+        } else if (i2 < 0) {
+            return compareFirst ? -1 : 1;
+        }
+        // i1 >= 0 && i2 >= 0, return explicit values in the order they are specified.
+        return i1 - i2;
+    }
+}
---
+++ src/test/java/nl/talsmasoftware/misc/utils/FirstLastComparatorTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2022 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.misc.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FirstLastComparatorTest {
+    @Test
+    void must_delegate_normal_without_values() {
+        // prepare
+        final Comparator<Character> delegate = Comparator.naturalOrder();
+        final String value = "cbda";
+
+        // execute
+        final String result1 = sortString(FirstLastComparator.compareFirst(delegate, emptyList()), value);
+        final String result2 = sortString(FirstLastComparator.compareLast(delegate, emptyList()), value);
+
+        // verify
+        assertThat(result1, equalTo("abcd"));
+        assertThat(result2, equalTo("abcd"));
+    }
+
+    @Test
+    void must_sort_single_value_first() {
+        // prepare
+        final Comparator<Character> subject = FirstLastComparator.compareFirst(Comparator.naturalOrder(), 'o');
+
+        // execute
+        final String result = sortString(subject, "The quick brown fox jumps over the lazy dog");
+
+        // verify
+        assertThat(result, equalTo("oooo        Tabcdeeefghhijklmnpqrrstuuvwxyz"));
+    }
+
+    @Test
+    void must_sort_single_value_last() {
+        // prepare
+        final Comparator<Character> subject = FirstLastComparator.compareLast(Comparator.reverseOrder(), 'o');
+
+        // execute
+        final String result = sortString(subject, "The quick brown fox jumps over the lazy dog");
+
+        // verify
+        assertThat(result, equalTo("zyxwvuutsrrqpnmlkjihhgfeeedcbaT        oooo"));
+    }
+
+    @Test
+    void first_values_are_in_given_order() {
+        // prepare
+        final Comparator<Character> subject = FirstLastComparator.compareFirst(Comparator.naturalOrder(), 'o', 'r', 'a');
+
+        // execute
+        final String result = sortString(subject, "The quick brown fox jumps over the lazy dog");
+
+        // verify
+        assertThat(result, equalTo("oooorra        Tbcdeeefghhijklmnpqstuuvwxyz"));
+    }
+
+    @Test
+    void last_values_are_in_given_order() {
+        // prepare
+        final Comparator<Character> subject = FirstLastComparator.compareLast(Comparator.naturalOrder(), 'o', 'r', 'a');
+
+        // execute
+        final String result = sortString(subject, "The quick brown fox jumps over the lazy dog");
+
+        // verify
+        assertThat(result, equalTo("        Tbcdeeefghhijklmnpqstuuvwxyzoooorra"));
+    }
+
+    @Test
+    void test_nullsafe_compare() {
+        // prepare
+        final List<String> values = Arrays.asList("zzzz", null, "last", "aaaa", "first", null, "    ");
+
+        // execute
+        List<String> result1 = sortCopy(FirstLastComparator.compareFirst(Comparator.nullsFirst(Comparator.naturalOrder()), "first"), values);
+        List<String> result2 = sortCopy(FirstLastComparator.compareFirst(Comparator.naturalOrder(), null, "first"), values);
+        List<String> result3 = sortCopy(FirstLastComparator.compareLast(Comparator.nullsLast(Comparator.naturalOrder()), "last"), values);
+        List<String> result4 = sortCopy(FirstLastComparator.compareLast(Comparator.naturalOrder(), "last", null), values);
+
+        // verify
+        assertThat(result1, contains("first", null, null, "    ", "aaaa", "last", "zzzz"));
+        assertThat(result2, contains(null, null, "first", "    ", "aaaa", "last", "zzzz"));
+        assertThat(result3, contains("    ", "aaaa", "first", "zzzz", null, null, "last"));
+        assertThat(result4, contains("    ", "aaaa", "first", "zzzz", "last", null, null));
+    }
+
+    @Test
+    void all_parameters_to_factorymethods_are_required() {
+        final Comparator<String> natural = Comparator.naturalOrder();
+        // Check nulls for compareFirst
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareFirst(null, "")),
+                hasProperty("message", equalTo("Delegate comparator is <null>.")));
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareFirst(natural, (String[]) null)),
+                hasProperty("message", equalTo("firstValues is <null>.")));
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareFirst(null, Collections.singleton(""))),
+                hasProperty("message", equalTo("Delegate comparator is <null>.")));
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareFirst(natural, (List<String>) null)),
+                hasProperty("message", equalTo("firstValues is <null>.")));
+
+        // Check nulls for compareLast
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareLast(null, "")),
+                hasProperty("message", equalTo("Delegate comparator is <null>.")));
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareLast(natural, (String[]) null)),
+                hasProperty("message", equalTo("lastValues is <null>.")));
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareLast(null, Collections.singleton(""))),
+                hasProperty("message", equalTo("Delegate comparator is <null>.")));
+        assertThat(assertThrows(NullPointerException.class, () -> FirstLastComparator.compareLast(natural, (List<String>) null)),
+                hasProperty("message", equalTo("lastValues is <null>.")));
+    }
+
+    static String sortString(Comparator<Character> comparator, String characters) {
+        char[] chars = characters.toCharArray();
+        List<Character> values = new ArrayList<>(chars.length);
+        for (char ch : chars) {
+            values.add(ch);
+        }
+        return sortCopy(comparator, values).stream().collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+    }
+
+    static <T> List<T> sortCopy(Comparator<? super T> comparator, Collection<? extends T> values) {
+        final List<T> sortedCopy = new ArrayList<>(values);
+        sortedCopy.sort(comparator);
+        return sortedCopy;
+    }
+}
